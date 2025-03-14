28e309961b0db2d019a5d260ef9a6dc8b10e3465
Liehan Els <liehan13@gmail.com>
2025-03-14 09:39:17 +0200

    Added UI

--- app/src/main/java/com/liehan/githubpractice/MainActivity.kt
+++ app/src/main/java/com/liehan/githubpractice/MainActivity.kt
@@ -1,6 +1,9 @@
 package com.liehan.githubpractice
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -15,6 +18,27 @@ class MainActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+        //get the number and button element from the layout
+        val input = findViewById<EditText>(R.id.edtNumber)
+        val clickMe = findViewById<Button>(R.id.btnCheckNum)
+        //set onClickListener for the button
+        clickMe.setOnClickListener {
+            //try catch statement to check if the input is a number
+            try {
+                //tries to turn the string into a number
+                val numberInt = input.text.toString().toInt()
+                //if the conversion succeeds, check if the number is even or odd
+                // % is the modulo operator, which returns the remainder of the division
+                if (numberInt % 2 == 0) {
+                    Toast.makeText(this, "The number $numberInt is even", Toast.LENGTH_LONG).show()
+                } else {
+                    Toast.makeText(this, "The number $numberInt is odd", Toast.LENGTH_LONG).show()
+                }
+                //if the conversion fails, show that the input is NaN
+            } catch (e: NumberFormatException) {
+                Toast.makeText(this, "The input $input is not a number", Toast.LENGTH_LONG).show()
+            }
         }
     }
 }
